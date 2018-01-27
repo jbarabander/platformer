f9d10754822cee71ab1bfd3a0bcc9c800d6e7851
Justin <jbarabander@gmail.com>
2018-01-27 17:06:57 -0500

    Working on dat jumping

---
+++ main.lua
@@ -0,0 +1,20 @@
+platform = {}
+ 
+function love.load()
+	platform.width = love.graphics.getWidth()
+	platform.height = love.graphics.getHeight()
+	platform.x = 0
+	platform.y = platform.height / 2
+    require "player"
+    player = Player()
+end
+ 
+function love.update(dt)
+    player:update(dt)
+end
+ 
+function love.draw()
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
+    player:draw()
+end
---
+++ player.lua
@@ -0,0 +1,87 @@
+Object = require "classic"
+math = require "math"
+Player = Object:extend()
+
+local MAX_NUM_OF_JUMPING_FRAMES = 20
+
+
+function Player:new()
+	self.speed = 200
+	self.y_velocity = 0 
+	self.jump_height = -300
+	self.gravity = -900
+    self.x = love.graphics.getWidth() / 2
+	self.y = love.graphics.getHeight() / 2 - 50
+    self.ground = self.y
+    self.size = 50
+    self.jumpingFrames = 0
+    self.hasDoubleJumped = false
+    self.doubleJumpHeight = self.jump_height / 2
+end
+
+function Player:draw()
+    love.graphics.rectangle("line", self.x, self.y, self.size, self.size)
+end
+
+function Player:canStillJump()
+    return (
+        self.y_velocity == 0 or 
+        self.jumpingFrames < MAX_NUM_OF_JUMPING_FRAMES
+    )
+end
+
+function Player:clearJump()
+    self.jumpingFrames = 0
+    self.y_velocity = 0
+    self.y = self.ground
+    self.hasDoubleJumped = false
+end
+
+function Player:doubleJump()
+    self.jumpingFrames = 0
+    self.y_velocity = self.doubleJumpHeight
+    self.hasDoubleJumped = true
+end
+
+function Player:reAddJumpHeight()
+    if not self.hasDoubleJumped then
+        self.y_velocity = self.doubleJumpHeight
+    else
+        self.y_velocity = self.jump_height
+    end
+end
+
+function Player:jumpingStatus(dt)
+	if love.keyboard.isDown('space') then
+        if self.y_velocity == 0 or self.jumpingFrames < MAX_NUM_OF_JUMPING_FRAMES then
+		    self:reAddJumpHeight()
+            self.jumpingFrames = self.jumpingFrames + 1
+        elseif not self.hasDoubleJumped and self.jumpingFrames == math.huge then 
+            self:doubleJump()
+        end
+	end 
+    if not love.keyboard.isDown('space') and self.y_velocity ~=0 then
+        self.jumpingFrames = math.huge
+    end
+	if self.y_velocity ~= 0 then
+		self.y = self.y + self.y_velocity * dt
+		self.y_velocity = self.y_velocity - self.gravity * dt
+	end
+end
+
+function Player:update(dt)
+    if love.keyboard.isDown('d') then
+		if self.x < (love.graphics.getWidth() - self.size) then
+			self.x = self.x + (self.speed * dt)
+		end
+	elseif love.keyboard.isDown('a') then
+		if self.x > 0 then 
+			self.x = self.x - (self.speed * dt)
+		end
+	end
+    self:jumpingStatus(dt)
+    -- collision detection
+    if self.y > self.ground then
+        self:clearJump()
+	end
+end
